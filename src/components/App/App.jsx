b26f3e22728839801cfa5e8e5e927fec3d19dc5b
Nina Vasilieva <aninok92@gmail.com>
2021-06-28 11:16:14 +0300

    Добавляет компонет Statistics, FriendList, Transaction

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,18 @@
 import { Profile } from "../Profile/Profile"
+import { Statistics } from "../Statistics/Statistics"
+import { FriendList } from "../Friends/FriendList/FriendList"
+import { TransactionHistory } from "../TransactionHistory/Transactions/Transactions"
 import user from "../../user.json"
+import statisticalData from "../../statistical-data.json"
+import friends from "../../friends.json"
+import transactions from '../../transactions.json';
 
 export const App = () => {
     return (<>
-     <Profile user = {user}/>
+        <Profile user={user} />
+        <Statistics title="Upload stats" stats={statisticalData} />;
+        <Statistics stats={statisticalData} />;
+        <FriendList friends={friends} />
+        <TransactionHistory items={transactions} />
  </>)
 }
