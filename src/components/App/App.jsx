a24cb4068a4625ef8af32fb0b6ad984244c61dc5
Nina Vasilieva <aninok92@gmail.com>
2021-06-29 14:49:28 +0300

    Убирает лишний компонет User

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,11 +8,21 @@ import friends from "../../friends.json"
 import transactions from '../../transactions.json';
 
 export const App = () => {
-    return (<>
-        <Profile user={user} />
-        <Statistics title="Upload stats" stats={statisticalData} />;
-        <Statistics stats={statisticalData} />;
-        <FriendList friends={friends} />
-        <TransactionHistory items={transactions} />
- </>)
+    return (
+        <>
+            <Profile
+                name={user.name}
+                tag={user.tag}
+                location={user.location}
+                avatar={user.avatar}
+                followers={user.stats.followers}
+                views={user.stats.views}
+                likes={user.stats.likes}
+            />
+            <Statistics title="Upload stats" stats={statisticalData} />;
+            <Statistics stats={statisticalData} />;
+            <FriendList friends={friends} />
+            <TransactionHistory items={transactions} />
+        </>
+    )
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,22 +1,45 @@
 import PropTypes from 'prop-types';
-import { User } from "./User"
+import { FaAt } from "react-icons/fa"
 import css from './Profile.module.css'
 
-export const Profile = ({ user })=> {
-  return (
-    <div className={css.profile}>
-      <User
-        name={user.name}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        followers={user.stats.followers}
-        views={user.stats.views}
-        likes={user.stats.likes}
-      />    
-    </div>)
+export const Profile = ({ name, tag, location, avatar, followers, views, likes }) => {
+    return (
+        <div className={css.profile}>
+        <div className={css.description}>
+          <img
+              src={avatar}
+              alt={name}
+              className={css.avatar}
+              width ="240"
+          />
+          <p className={css.name}>{name}</p>
+          <p className={css.tag}><FaAt className={css.icon} size = "18"/>{ tag }</p>
+          <p className={css.location}> {location}</p>
+        </div>
+        <ul className={css.stats}>
+          <li>
+            <span className={css.label}>Followers</span>
+            <span className = {css.quantity}>{ followers}</span>
+          </li>
+          <li>
+            <span className={css.label}>Views</span>
+            <span className={css.quantity}>{views}</span>
+          </li>
+          <li>
+            <span className={css.label}>Likes</span>
+            <span className={css.quantity}>{likes}</span>
+          </li>
+        </ul>
+      </div>
+    )
 }
 
 Profile.propTypes = {
-  user:  PropTypes.object.isRequired
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  followers: PropTypes.number.isRequired,
+  views: PropTypes.number.isRequired,
+  likes: PropTypes.number.isRequired,
 }
